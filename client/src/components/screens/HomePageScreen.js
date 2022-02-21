ebfda11380cd09ec4dcaa9215940e8c9e6d09b18
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-02-22 01:14:35 +0530

    smaller changes

--- client/src/components/screens/HomePageScreen.js
+++ client/src/components/screens/HomePageScreen.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import * as Bootstrap from "react-bootstrap";
 
 const HomePageScreen = () => {
 
@@ -21,10 +22,30 @@ const HomePageScreen = () => {
     };
 
     return (
+
         <div>
-            <div style={{ background: "green", color: "white" }}>Welcome</div>
-            <button onClick={logoutHandler}>Logout</button>
-            <button onClick={navigateHandler}>Edit Profile</button>
+            <Bootstrap.Navbar bg="primary" expand="lg" variant="dark">
+                <Bootstrap.Container>
+                    <Bootstrap.Navbar.Brand href="/">Welcome User</Bootstrap.Navbar.Brand>
+                    <Bootstrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Bootstrap.Navbar.Collapse id="basic-navbar-nav">
+                        <Bootstrap.Nav className="me-auto">
+                            <Bootstrap.NavDropdown title="Profile" id="basic-nav-dropdown">
+                                <Bootstrap.NavDropdown.Item href="/editprofile">Edit Profile</Bootstrap.NavDropdown.Item>
+                            </Bootstrap.NavDropdown>
+                        </Bootstrap.Nav>
+                        <Bootstrap.Nav className="me-auto">
+                            <Bootstrap.Nav.Link onClick={logoutHandler}>Logout</Bootstrap.Nav.Link>
+                        </Bootstrap.Nav>
+                    </Bootstrap.Navbar.Collapse>
+                </Bootstrap.Container>
+            </Bootstrap.Navbar>
+            <br></br>
+            <div className="d-grid gap-2">
+                <Bootstrap.Button variant="secondary" size="sm" onClick={navigateHandler}>Edit Profile</Bootstrap.Button>
+                <Bootstrap.Button variant="danger" size="sm" onClick={logoutHandler}>Logout</Bootstrap.Button>
+            </div>
+            
         </div>
     );
 };
